a590883ac09688e7cc5243fa854ee56bf4f773f2
Octavio <Octavio@Octavio-PC>
2018-09-29 18:35:47 +0200

    crear proyecto

---
+++ src/main/java/org/octavio/bibliotecams/BibliotecaMsApplication.java
@@ -0,0 +1,12 @@
+package org.octavio.bibliotecams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaMsApplication.class, args);
+	}
+}
